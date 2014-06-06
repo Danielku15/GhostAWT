83ddb3a8b51f9de5fe7756f277ffbb8442996a9e
ubuntu <ubuntu@ubuntu64.(none)>
2014-06-06 22:27:53 +0200

    Added 64bit linux build

--- jni/commons.h
+++ jni/commons.h
@@ -2,6 +2,7 @@
 #define COMMONS_H_
 
 #include <jni.h>
+#include <stdint.h>
 
 #define INITIDS_STUB(classname) \
 JNIEXPORT void JNICALL \
@@ -9,22 +10,12 @@ Java_##classname##_initIDs(JNIEnv *env, jclass cls) \
 { \
 }
 
-#ifdef _LP64
+#if defined(__LP__) || defined(_LP64)
 #define jlong_to_ptr(a) ((void*)(a))
 #define ptr_to_jlong(a) ((jlong)(a))
 #else
 #define jlong_to_ptr(a) ((void*)(int)(a))
 #define ptr_to_jlong(a) ((jlong)(int)(a))
 #endif
-
-# ifndef _UINTPTR_T_DEFINED
-#  define _UINTPTR_T_DEFINED
-#  ifdef _LP64
-     typedef unsigned __int64 uintptr_t;
-#  else
-     typedef unsigned int uintptr_t;
-#  endif
-# endif
-
 
 #endif /* COMMONS_H_ */
